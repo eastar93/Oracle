de3c9fb14d9854f4c560ee7a31a0a59081d9475a
eastar93 <eastar93@naver.com>
2021-09-24 19:37:47 +0900

    eastar93

--- 20210923Board_tbl.sql
+++ 20210923Board_tbl.sql
@@ -5,13 +5,13 @@ alter table board_tbl add constraint pk_board primary key(bno);
     힌트구문은 아래처럼 주석처럼 쓰되, 여는 부분에 +를 추가로 붙입니다.
     힌트는 FULL, INDEX_DESC, INDEX_ASC등이 있습니다. */
 
-SELECT /* + INDEX_DESC (board_tbl pk_board) */ * from board_tbl;
+SELECT /*+ INDEX_DESC (board_tbl pk_board) */ * from board_tbl;
     
 SELECT * FROM board_tbl ORDER BY bno DESC;
 
 SELECT rownum, rowid, bno from board_tbl;
 
-SELECT /* + INDEX_DESC(board_tbl pk_board) */ 
+SELECT /*+ INDEX_DESC(board_tbl pk_board) */ 
     ROWNUM rn, board_tbl.* 
         FROM board_tbl WHERE ROWNUM <= 20;
 
@@ -19,12 +19,24 @@ SELECT bno, title, content, writer, regdate, updatedate
     FROM (SELECT /* + INDEX_DESC(board_tbl pk_board) */ 
         rownum, bno, title, content, writer, regdate, updatedate 
             FROM board_tbl WHERE ROWNUM <= 20) WHERE ROWNUM > 10;
-
+-- 15페이지
 SELECT ROWNUM, bt.* FROM
     (SELECT /*+ INDEX_DESC(board_tbl pk_board) */ 
         ROWNUM rn, board_tbl.* 
-            FROM board_tbl WHERE ROWNUM <= 20) bt 
-                WHERE rn > 10;
+            FROM board_tbl WHERE ROWNUM <= 15*10) bt 
+                WHERE rn > (15-1)*10;
+-- 30페이지                
+SELECT ROWNUM, bt.* FROM
+    (SELECT /*+ INDEX_DESC(board_tbl pk_board) */ 
+        ROWNUM rn, board_tbl.* 
+            FROM board_tbl WHERE ROWNUM <= (30*10)) bt 
+                WHERE rn > (30-1)*10;
+-- 426페이지                
+SELECT ROWNUM, bt.* FROM
+    (SELECT /*+ INDEX_DESC(board_tbl pk_board) */ 
+        ROWNUM rn, board_tbl.* 
+            FROM board_tbl WHERE ROWNUM <= (426*10)) bt 
+                WHERE rn > (426-1)*10;
 
 SELECT * FROM board_tbl ORDER BY bno DESC;
 
