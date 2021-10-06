647824d49032d0877b782e32bef1d454994bc92b
eastar93 <eastar93@naver.com>
2021-10-06 14:52:27 +0900

    eastar93

---
+++ tbl_test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tbl_test1(col1 varchar(50));
+CREATE TABLE tbl_test2(col2 varchar(5));
+
+select * from tbl_test1;
+select * from tbl_test2;
+
+DROP TABLE tbl_test1;
+DROP TABLE tbl_test2;
