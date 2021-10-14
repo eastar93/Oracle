4700aba20ef8ac593e024c9fb80ac490440070e0
eastar93 <eastar93@naver.com>
2021-10-14 20:43:36 +0900

    eastar93

---
+++ 20211014.sql
@@ -0,0 +1,11 @@
+CREATE TABLE img_tbl(
+    uuid varchar(100) not null,
+    uploadPath varchar2(200) not null,
+    fileName varchar2(100) not null,
+    filetype char(1) default 'I',
+    bno number(10, 0)
+    );
+
+alter table img_tbl add constraint pk_img primary key (uuid);
+alter table img_tbl add constraint fk_board_img foreign key (bno) references board_tbl (bno);
+    
